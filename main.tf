5dbcbc23eea69831004b13bfad08b54d3661a40f
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-08-24 12:28:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,3 +81,84 @@ resource "aws_instance" "web" {
     Name = "Terraform-EC2"
   }
 }
+
+# 10. Random suffix for unique S3 bucket name
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# 11. S3 Bucket for VPC Flow Logs (unencrypted)
+resource "aws_s3_bucket" "flow_logs_bucket" {
+  bucket = "my-flow-logs-bucket-${random_id.suffix.hex}"
+}
+
+# 12. Get current AWS account ID
+data "aws_caller_identity" "current" {}
+
+# 13. Bucket policy to allow VPC Flow Logs service to write logs
+resource "aws_s3_bucket_policy" "flow_logs_policy" {
+  bucket = aws_s3_bucket.flow_logs_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AWSLogDeliveryWrite"
+        Effect = "Allow"
+        Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.flow_logs_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid = "AWSLogDeliveryAclCheck"
+        Effect = "Allow"
+        Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.flow_logs_bucket.arn
+      }
+    ]
+  })
+}
+
+# 14. VPC Flow Log
+resource "aws_flow_log" "vpc_flow_log" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
+  log_destination      = aws_s3_bucket.flow_logs_bucket.arn
+  log_destination_type = "s3"
+  max_aggregation_interval = 600
+
+  # Terraform-compatible log format using heredoc
+  log_format = <<EOT
+version account-id interface-id srcaddr dstaddr srcport dstport protocol packets bytes start end action log-status
+EOT
+
+  depends_on = [aws_s3_bucket_policy.flow_logs_policy]
+}
+
+# --- Outputs for verification ---
+
+output "flow_logs_bucket_name" {
+  description = "The name of the S3 bucket storing VPC flow logs"
+  value       = aws_s3_bucket.flow_logs_bucket.id
+}
+
+output "flow_logs_bucket_arn" {
+  description = "The ARN of the S3 bucket storing VPC flow logs"
+  value       = aws_s3_bucket.flow_logs_bucket.arn
+}
+
+output "vpc_flow_log_id" {
+  description = "The ID of the created VPC Flow Log"
+  value       = aws_flow_log.vpc_flow_log.id
+}
+
+output "vpc_flow_log_status" {
+  description = "The current status of the VPC Flow Log"
+  value       = aws_flow_log.vpc_flow_log.flow_log_status
+}
