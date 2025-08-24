6a389df5ee4aac301a1c5ba4f11931264166c3ec
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-08-24 11:36:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,16 +82,20 @@ resource "aws_instance" "web" {
   }
 }
 
-# 10. S3 Bucket for Flow Logs (unencrypted)
-resource "aws_s3_bucket" "flow_logs_bucket" {
-  bucket = "my-flow-logs-bucket-${random_id.suffix.hex}"
-}
-
+# 10. Random suffix for unique bucket name
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# 11. Bucket policy to allow VPC Flow Logs service to write logs
+# 11. S3 Bucket for Flow Logs (unencrypted)
+resource "aws_s3_bucket" "flow_logs_bucket" {
+  bucket = "my-flow-logs-bucket-${random_id.suffix.hex}"
+}
+
+# 12. Current AWS Account ID
+data "aws_caller_identity" "current" {}
+
+# 13. Bucket policy to allow VPC Flow Logs service to write logs
 resource "aws_s3_bucket_policy" "flow_logs_policy" {
   bucket = aws_s3_bucket.flow_logs_bucket.id
 
@@ -99,23 +103,34 @@ resource "aws_s3_bucket_policy" "flow_logs_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid = "AWSLogDeliveryWrite"
         Effect = "Allow"
-        Principal = {
-          Service = "vpc-flow-logs.amazonaws.com"
-        }
+        Principal = { Service = "vpc-flow-logs.amazonaws.com" }
         Action   = "s3:PutObject"
-        Resource = "${aws_s3_bucket.flow_logs_bucket.arn}/*"
+        Resource = "${aws_s3_bucket.flow_logs_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid = "AWSLogDeliveryAclCheck"
+        Effect = "Allow"
+        Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.flow_logs_bucket.arn
       }
     ]
   })
 }
 
-# 12. VPC Flow Log
+# 14. VPC Flow Log
 resource "aws_flow_log" "vpc_flow_log" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
   log_destination      = aws_s3_bucket.flow_logs_bucket.arn
   log_destination_type = "s3"
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.main.id
 
   log_format = "version account-id interface-id srcaddr dstaddr srcport dstport protocol packets bytes start end action log-status"
 
