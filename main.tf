4196cf17e6143041ece728a4bee5fce857a5329e
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-08-28 10:41:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -230,15 +230,15 @@ resource "aws_s3_bucket_policy" "flow_logs_policy" {
 ###############################
 # 14. VPC Flow Log
 ###############################
-resource "aws_flow_log" "vpc_flow_log" {
-  vpc_id               = aws_vpc.main.id
-  traffic_type         = "ALL"
-  log_destination      = aws_s3_bucket.flow_logs_bucket.arn
-  log_destination_type = "s3"
-  max_aggregation_interval = 60
-
-  depends_on = [aws_s3_bucket_policy.flow_logs_policy]
-}
+#resource "aws_flow_log" "vpc_flow_log" {
+#  vpc_id               = aws_vpc.main.id
+#  traffic_type         = "ALL"
+#  log_destination      = aws_s3_bucket.flow_logs_bucket.arn
+#  log_destination_type = "s3"
+#  max_aggregation_interval = 60
+#
+#  depends_on = [aws_s3_bucket_policy.flow_logs_policy]
+#}
 
 ###############################
 # 15. Generate local key file
